9c4689d0eb01ed3bc7ce9d594253bbcf588d5c49
Veera Itälinna <veera.italinna@gmail.com>
2017-11-30 23:41:47 +0200

    added Box2D Testbed for testing

---
+++ src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/DependInfo.cmake
@@ -0,0 +1,66 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Collision/Shapes/b2ChainShape.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Collision/Shapes/b2ChainShape.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Collision/Shapes/b2CircleShape.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Collision/Shapes/b2CircleShape.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Collision/Shapes/b2EdgeShape.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Collision/Shapes/b2EdgeShape.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Collision/Shapes/b2PolygonShape.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Collision/Shapes/b2PolygonShape.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Collision/b2BroadPhase.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Collision/b2BroadPhase.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Collision/b2CollideCircle.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Collision/b2CollideCircle.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Collision/b2CollideEdge.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Collision/b2CollideEdge.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Collision/b2CollidePolygon.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Collision/b2CollidePolygon.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Collision/b2Collision.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Collision/b2Collision.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Collision/b2Distance.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Collision/b2Distance.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Collision/b2DynamicTree.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Collision/b2DynamicTree.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Collision/b2TimeOfImpact.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Collision/b2TimeOfImpact.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Common/b2BlockAllocator.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Common/b2BlockAllocator.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Common/b2Draw.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Common/b2Draw.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Common/b2Math.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Common/b2Math.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Common/b2Settings.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Common/b2Settings.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Common/b2StackAllocator.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Common/b2StackAllocator.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Common/b2Timer.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Common/b2Timer.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Dynamics/Contacts/b2ChainAndCircleContact.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Dynamics/Contacts/b2ChainAndCircleContact.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Dynamics/Contacts/b2ChainAndPolygonContact.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Dynamics/Contacts/b2ChainAndPolygonContact.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Dynamics/Contacts/b2CircleContact.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Dynamics/Contacts/b2CircleContact.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Dynamics/Contacts/b2Contact.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Dynamics/Contacts/b2Contact.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Dynamics/Contacts/b2ContactSolver.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Dynamics/Contacts/b2ContactSolver.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Dynamics/Contacts/b2EdgeAndCircleContact.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Dynamics/Contacts/b2EdgeAndCircleContact.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Dynamics/Contacts/b2EdgeAndPolygonContact.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Dynamics/Contacts/b2EdgeAndPolygonContact.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Dynamics/Contacts/b2PolygonAndCircleContact.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Dynamics/Contacts/b2PolygonAndCircleContact.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Dynamics/Contacts/b2PolygonContact.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Dynamics/Contacts/b2PolygonContact.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Dynamics/Joints/b2DistanceJoint.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2DistanceJoint.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Dynamics/Joints/b2FrictionJoint.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2FrictionJoint.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Dynamics/Joints/b2GearJoint.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2GearJoint.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Dynamics/Joints/b2Joint.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2Joint.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Dynamics/Joints/b2MotorJoint.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2MotorJoint.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Dynamics/Joints/b2MouseJoint.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2MouseJoint.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Dynamics/Joints/b2PrismaticJoint.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2PrismaticJoint.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Dynamics/Joints/b2PulleyJoint.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2PulleyJoint.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Dynamics/Joints/b2RevoluteJoint.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2RevoluteJoint.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Dynamics/Joints/b2RopeJoint.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2RopeJoint.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Dynamics/Joints/b2WeldJoint.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2WeldJoint.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Dynamics/Joints/b2WheelJoint.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2WheelJoint.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Dynamics/b2Body.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Dynamics/b2Body.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Dynamics/b2ContactManager.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Dynamics/b2ContactManager.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Dynamics/b2Fixture.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Dynamics/b2Fixture.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Dynamics/b2Island.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Dynamics/b2Island.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Dynamics/b2World.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Dynamics/b2World.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Dynamics/b2WorldCallbacks.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Dynamics/b2WorldCallbacks.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Box2D/Rope/b2Rope.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/Rope/b2Rope.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../Box2D/.."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/Box2D/Build/Testbed/CMakeFiles/Testbed.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Testbed/Framework/Main.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Testbed/CMakeFiles/Testbed.dir/Framework/Main.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Testbed/Framework/Render.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Testbed/CMakeFiles/Testbed.dir/Framework/Render.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Testbed/Framework/Test.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Testbed/CMakeFiles/Testbed.dir/Framework/Test.cpp.o"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Testbed/Tests/TestEntries.cpp" "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Testbed/CMakeFiles/Testbed.dir/Tests/TestEntries.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "FREEGLUT_EXPORTS"
+  "FREEGLUT_STATIC"
+  "_CRT_SECURE_NO_WARNINGS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/Box2D/CMakeFiles/Box2D.dir/DependInfo.cmake"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/freeglut/CMakeFiles/freeglut_static.dir/DependInfo.cmake"
+  "/home/veeraitalinna/hill-racing-1/src/Box2D/Build/glui/CMakeFiles/glui.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
