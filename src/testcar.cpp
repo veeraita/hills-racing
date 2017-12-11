d66d6ae3f343a4862d062d8335f64f1e53366536
Oskari Lehtonen <oskari.lehtonen@aalto.fi>
2017-12-12 00:27:26 +0200

    new joints created, next up debug drawing

--- src/testcar.cpp
+++ src/testcar.cpp
@@ -7,17 +7,13 @@
 
 Testcar::Testcar( b2World& world, int MouseX, int MouseY ) : world(world), MouseX(MouseX), MouseY(MouseY)
 {
-  /*========================== BOX2D SETUP FOR CAR =================================================*/
+/*========================== BOX2D SETUP FOR CAR =================================================*/
       //Random variables for the joints
       float wheeldistance = 2.3f;
       float m_zeta = 0.7f;
 
-  /*=========================== CHASSIS = RUNKO ====================================================*/
-      b2BodyDef bd_chassis;
-      bd_chassis.type = b2_dynamicBody;
-      bd_chassis.userData = (void*)"chassis";
-      bd_chassis.position = b2Vec2( MouseX/SCALE, MouseY/SCALE ); //origin of chassis
-
+/*=========================== CHASSIS = RUNKO ====================================================*/
+      //This shape is used in Car.h testbed
       // b2PolygonShape chassis;
       // b2Vec2 vertices[8];
       // vertices[0].Set(2.0f, 11.0f);
@@ -27,98 +23,162 @@ Testcar::Testcar( b2World& world, int MouseX, int MouseY ) : world(world), Mouse
       // vertices[4].Set(2.4f, 12.21f);
       // vertices[5].Set(2.0f, 11.8f);
       // chassis.Set(vertices, 6);
+
       b2PolygonShape chassis;
-      chassis.SetAsBox( (100.0f/2.0f)/SCALE, (36.33f/2.0f)/SCALE );
-      b2Body* car = world.CreateBody(&bd_chassis);
+      chassis.SetAsBox( (100.0f/2.0f)/SCALE, (36.33f/2.0f)/SCALE );//Sprite: width=100px, height=36.33px
 
       b2FixtureDef fd_chassis;
       fd_chassis.density = 1.0;
+      fd_chassis.filter.groupIndex = -1;
       fd_chassis.shape = &chassis;
+
+      b2BodyDef bd_chassis;
+      bd_chassis.type = b2_dynamicBody;
+      bd_chassis.userData = (void*)"chassis";
+      bd_chassis.position = b2Vec2( MouseX/SCALE, MouseY/SCALE ); //origin of the chassis
+
+      b2Body* car = world.CreateBody( &bd_chassis );
       car->CreateFixture( &fd_chassis );
 
-  /*=========================== WHEELS ==============================================================*/
-      b2CircleShape circle;
-      circle.m_radius = 11.66f/SCALE;
+/*=========================== AXLES = AKSELIT =====================================================*/
+      b2PolygonShape axleshape;//axles will be regular square boxes
+      axleshape.SetAsBox( (11.66f/2.0f)/SCALE, (11.66f/2.0f)/SCALE );//Need to fit inside wheels
 
-      /*========== BACK WHEEL ================*/
+      b2FixtureDef fd_axle;
+      fd_axle.density = 0.5f;
+      fd_axle.friction = 0.9f;
+      fd_axle.restitution = 0.3f;
+      fd_axle.filter.groupIndex = -1;
+      fd_axle.shape = &axleshape;
+
+      b2BodyDef bd_axle;
+      bd_axle.type = b2_dynamicBody;
+      bd_axle.userData = (void*)"axle";
+      bd_axle.position.Set( (MouseX/SCALE)-0.3f, (MouseY/SCALE)+0.9f  );//car->GetWorldCenter().x-(35.66f/SCALE), car->GetWorldCenter().y+0.9f
+
+      /*===================== REAR AXLE ===========================*/
+      b2Body* rearAxle = world.CreateBody( &bd_axle );
+      rearAxle->CreateFixture( &fd_axle );
+
+      /*===================== FRONT AXLE ===========================*/
+      bd_axle.position.Set( (MouseX/SCALE)+2.05f, (MouseY/SCALE)+0.9f );//car->GetWorldCenter().x+(33.66f/SCALE), car->GetWorldCenter().y+0.9f
+      b2Body* frontAxle = world.CreateBody( &bd_axle );
+      frontAxle->CreateFixture( &fd_axle );
+
+/*=========================== WHEELS ==============================================================*/
+      b2CircleShape circle;
+      circle.m_radius = 11.66f/SCALE;//WheelSprite: half-width=11.66px
+
+      b2FixtureDef fd_wheel1;
+      fd_wheel1.density = 1.0f;
+      fd_wheel1.friction = 1.9f;
+      fd_wheel1.filter.groupIndex = -1;
+      fd_wheel1.shape = &circle;
+
+      /*========== REAR WHEEL ================*/
       b2BodyDef bd_wheel1;
       bd_wheel1.type = b2_dynamicBody;
       bd_wheel1.userData = (void*)"wheel1";
-      bd_wheel1.position.Set( (MouseX/SCALE)-0.3f, (MouseY/SCALE)+0.9f );
-      b2Body* wheel1 = world.CreateBody( &bd_wheel1 );
+      bd_wheel1.position.Set( (MouseX/SCALE)-0.3f, (MouseY/SCALE)+0.9f );//(MouseX/SCALE)-0.3f (MouseY/SCALE)+0.9f
 
-      b2FixtureDef fd_wheel1;
-      fd_wheel1.shape = &circle;
-      fd_wheel1.density = 1.0f;
-      fd_wheel1.friction = 0.9f;
+      b2Body* wheel1 = world.CreateBody( &bd_wheel1 );
       wheel1->CreateFixture( &fd_wheel1 );
 
       /*=========== FRONT WHEEL ===============*/
+      b2FixtureDef fd_wheel2;
+      fd_wheel2.density = 1.0f;
+      fd_wheel2.friction = 1.9f;
+      fd_wheel2.filter.groupIndex = -1;
+      fd_wheel2.shape = &circle;
+
       b2BodyDef bd_wheel2;
       bd_wheel2.type = b2_dynamicBody;
       bd_wheel2.userData = (void*)"wheel2";
-      bd_wheel2.position.Set( (MouseX/SCALE)+2.05f, (MouseY/SCALE)+0.9f ); //frontwheel
-      b2Body* wheel2 = world.CreateBody( &bd_wheel2 );
+      bd_wheel2.position.Set( (MouseX/SCALE)+2.05f, (MouseY/SCALE)+0.9f );//(MouseX/SCALE)+2.05f (MouseY/SCALE)+0.9f
 
-      b2FixtureDef fd_wheel2;
-      fd_wheel2.shape = &circle;
-      fd_wheel2.density = 1.0f;
-      fd_wheel2.friction = 0.9f;
+      b2Body* wheel2 = world.CreateBody( &bd_wheel2 );
       wheel2->CreateFixture( &fd_wheel2 );
 
-  /*========================== JOINTS =========================================================*/
+/*========================== JOINTS =======================================================================*/
+  /*========= REVOLUTE JOINTS ===================*/
+      /*===== REAR =============*/
+      b2RevoluteJointDef rear_rjd;
+      rear_rjd.Initialize( wheel1, rearAxle, wheel1->GetWorldCenter() );
+      rear_rjd.enableMotor = true;
+      rear_rjd.maxMotorTorque = 30.0f;
+      rear_revoluter = (b2RevoluteJoint*)world.CreateJoint( &rear_rjd );
+
+      /*===== FRONT ==============*/
+      b2RevoluteJointDef front_rjd;
+      front_rjd.Initialize( wheel2, frontAxle, wheel2->GetWorldCenter() );
+      front_rjd.enableMotor = true;
+      front_rjd.maxMotorTorque = 30.0f;
+      front_revoluter = (b2RevoluteJoint*)world.CreateJoint( &front_rjd );
 
   /*========= PRISMATIC JOINTS ==================*/
-      // b2PrismaticJointDef pjd1;
-      // pjd1.bodyA = wheel1;
-      // pjd1.bodyB = car;
-      // pjd1.collideConnected = false;
-      // pjd1.localAxisA = b2Vec2( 0, 1 );
-      // pjd1.localAnchorA = wheel1->GetPosition();
-      // pjd1.localAnchorB = b2Vec2( car->GetPosition().x-0.57, car->GetPosition().y );
-      // b2PrismaticJoint* damper1 = (b2PrismaticJoint*)world.CreateJoint( &pjd1 );
-      //
-      // b2PrismaticJointDef pjd2;
-      // pjd2.bodyA = wheel2;
-      // pjd2.bodyB = car;
-      // pjd2.collideConnected = false;
-      // pjd2.localAxisA = b2Vec2( 0, 1 );
-      // pjd2.localAnchorA = wheel2->GetPosition();
-      // pjd2.localAnchorB = b2Vec2( car->GetPosition().x+4.0, car->GetPosition().y );
-      // b2PrismaticJoint* damper2 = (b2PrismaticJoint*)world.CreateJoint( &pjd2 );
+      b2Vec2 axis1( 0, 1 );
+      b2PrismaticJointDef pjd;
+      pjd.lowerTranslation = -5/SCALE;
+      pjd.upperTranslation = 4/SCALE;
+      pjd.enableLimit = true;
+      pjd.enableMotor = true;
+
+      /*===== REAR ===================*/
+      pjd.Initialize( car, frontAxle, frontAxle->GetWorldCenter(), axis1 );
+      rear_prismatic = (b2PrismaticJoint*)world.CreateJoint( &pjd );
+
+      /*===== FRONT ==================*/
+      pjd.Initialize( car, rearAxle, rearAxle->GetWorldCenter(), axis1 );
+      rear_prismatic = (b2PrismaticJoint*)world.CreateJoint( &pjd );
 
   /*========= WHEEL JOINTS ======================*/
-      b2WheelJointDef jd;
-      b2Vec2 axis( MouseX/SCALE, MouseY/SCALE ); //origin of chassis
-      axis.Normalize();
-      //wheeljoints connect the origin of the chassis and origin of circleshapes
-      jd.Initialize(car, wheel1, wheel1->GetPosition(), axis );
-      jd.motorSpeed = 0.0f;
-      jd.maxMotorTorque = 20.0f;
-      jd.enableMotor = true;
-      jd.frequencyHz = m_hz;
-      jd.dampingRatio = m_zeta;
-      spring1 = (b2WheelJoint*)world.CreateJoint(&jd);
+      // b2WheelJointDef jd;
+      // b2Vec2 axis( MouseX/SCALE, MouseY/SCALE ); //origin of chassis
+      // axis.Normalize();
+      // //wheeljoints connect the origin of the chassis and origin of circleshapes
+      // jd.Initialize(car, wheel1, wheel1->GetPosition(), axis );
+      // jd.motorSpeed = 0.0f;
+      // jd.maxMotorTorque = 20.0f;
+      // jd.enableMotor = true;
+      // jd.frequencyHz = m_hz;
+      // jd.dampingRatio = m_zeta;
+      // spring1 = (b2WheelJoint*)world.CreateJoint(&jd);
+      //
+      // jd.Initialize(car, wheel2, wheel2->GetPosition(), axis );
+      // jd.motorSpeed = 0.0f;
+      // jd.maxMotorTorque = 10.0f;
+      // jd.enableMotor = false;
+      // jd.frequencyHz = m_hz;
+      // jd.dampingRatio = m_zeta;
+      // spring2 = (b2WheelJoint*)world.CreateJoint(&jd);
 
-      jd.Initialize(car, wheel2, wheel2->GetPosition(), axis );
-      jd.motorSpeed = 0.0f;
-      jd.maxMotorTorque = 10.0f;
-      jd.enableMotor = false;
-      jd.frequencyHz = m_hz;
-      jd.dampingRatio = m_zeta;
-      spring2 = (b2WheelJoint*)world.CreateJoint(&jd);
-
-  /*========================== BOX2Dsetup End ==========================================*/
+/*========================== BOX2Dsetup End ==========================================*/
 }
 
 /*============================ GETTERS ========================================================*/
-b2WheelJoint* Testcar::GetSpring1()
+// b2WheelJoint* Testcar::GetSpring1()
+// {
+//   return spring1;
+// }
+// b2WheelJoint* Testcar::GetSpring2()
+// {
+//   return spring2;
+// }
+b2RevoluteJoint* Testcar::GetRearRevoluter()
 {
-  return spring1;
+  return rear_revoluter;
 }
-b2WheelJoint* Testcar::GetSpring2()
+b2RevoluteJoint* Testcar::GetFrontRevoluter()
 {
-  return spring2;
+  return front_revoluter;
+}
+b2PrismaticJoint* Testcar::GetRearPrismatic()
+{
+  return rear_prismatic;
+}
+b2PrismaticJoint* Testcar::GetFrontPrismatic()
+{
+  return front_prismatic;
 }
 float Testcar::GetMhz()
 {
@@ -132,41 +192,37 @@ void Testcar::Keyboard( sf::Event event )
   {
     if( event.key.code == sf::Keyboard::Left )
     {
-       GetSpring1()->SetMotorSpeed(-50.0f);
+      GetRearRevoluter()->SetMotorSpeed(-50.0f);
     }
-  }
-  if( event.type == sf::Event::KeyPressed )
-  {
     if( event.key.code == sf::Keyboard::Down )
     {
-      GetSpring1()->SetMotorSpeed(0.0f);
+      GetRearRevoluter()->SetMotorSpeed(0.0f);
     }
-  }
-  if( event.type == sf::Event::KeyPressed )
-  {
     if( event.key.code == sf::Keyboard::Right )
     {
-      GetSpring1()->SetMotorSpeed(50.0);
+      GetRearRevoluter()->SetMotorSpeed(50.0);
     }
   }
-  if( event.type == sf::Event::KeyPressed )
-  {
-    if( event.key.code == sf::Keyboard::Left && event.key.code == sf::Keyboard::Up )
-    {
-      float hz = GetMhz();
-      hz = b2Max(0.0f, hz - 1.0f);
-  	  GetSpring1()->SetSpringFrequencyHz(hz);
-  	  GetSpring2()->SetSpringFrequencyHz(hz);
-    }
-  }
-  if( event.type == sf::Event::KeyPressed )
-  {
-    if( event.key.code == sf::Keyboard::Right && event.key.code == sf::Keyboard::Up )
-    {
-      float hz = GetMhz();
-      hz += 1.0f;
-  	  GetSpring1()->SetSpringFrequencyHz(hz);
-  	  GetSpring2()->SetSpringFrequencyHz(hz);
-    }
-  }
+
+
+//   if( event.type == sf::Event::KeyPressed )
+//   {
+//     if( event.key.code == sf::Keyboard::Left && event.key.code == sf::Keyboard::Up )
+//     {
+//       float hz = GetMhz();
+//       hz = b2Max(0.0f, hz - 1.0f);
+//   	  GetSpring1()->SetSpringFrequencyHz(hz);
+//   	  GetSpring2()->SetSpringFrequencyHz(hz);
+//     }
+//   }
+//   if( event.type == sf::Event::KeyPressed )
+//   {
+//     if( event.key.code == sf::Keyboard::Right && event.key.code == sf::Keyboard::Up )
+//     {
+//       float hz = GetMhz();
+//       hz += 1.0f;
+//   	  GetSpring1()->SetSpringFrequencyHz(hz);
+//   	  GetSpring2()->SetSpringFrequencyHz(hz);
+//     }
+//   }
 }
--- src/testcar.hpp
+++ src/testcar.hpp
@@ -6,15 +6,24 @@ class Testcar
 {
 public:
   Testcar( b2World& world, int MouseX, int MouseY );
-  b2WheelJoint* GetSpring1();
-  b2WheelJoint* GetSpring2();
+  //b2WheelJoint* GetSpring1();
+  //b2WheelJoint* GetSpring2();
+  b2RevoluteJoint* GetRearRevoluter();
+  b2RevoluteJoint* GetFrontRevoluter();
+  b2PrismaticJoint* GetRearPrismatic();
+  b2PrismaticJoint* GetFrontPrismatic();
   float GetMhz();
   void Keyboard( sf::Event event );
+
 private:
   b2World& world;
   int MouseX;
   int MouseY;
   b2WheelJoint* spring1;
   b2WheelJoint* spring2;
+  b2RevoluteJoint* rear_revoluter;
+  b2RevoluteJoint* front_revoluter;
+  b2PrismaticJoint* rear_prismatic;
+  b2PrismaticJoint* front_prismatic;
   float m_hz = 5.0f;
 };
