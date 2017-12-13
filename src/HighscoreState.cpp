e80624f0315d9bc5c24fdd1fee074006832f2dc4
Markus Toivonen <markus.toivonen@aalto.fi>
2017-12-13 12:17:32 +0200

    highscore screen try, not working atm

---
+++ src/HighscoreState.cpp
@@ -0,0 +1,47 @@
+#pragma once
+#include <sstream>
+#include "SplashState.hpp"
+#include "MainMenuState.hpp"
+#include "MainMenuState.cpp"
+#include "DEFINITIONS.hpp"
+#include <iostream>
+
+namespace Hills
+{
+	HighscoreState::HighscoreState( GameDataRef data) : _data( data )
+	{
+
+	}
+
+	void HighscoreState::Init()
+	{
+		this->_data->assets.LoadTexture( "Splash State Background", SPLASH_SCENE_BACKGROUND_FILEPATH );
+		this->_background.setTexture( this->_data->assets.GetTexture( "Splash State Background") );
+	}
+
+	void HighscoreState::HandleInput( )
+	{
+		sf::Event event;
+
+		while ( this->_data->window.pollEvent( event ) )
+		{
+			if ( sf::Event::Closed == event.type )
+			{
+				this->_data->window.close();
+			}
+		}
+	}
+
+	void HighscoreState::Update( float dt )
+	{
+
+	}
+
+	void HighscoreState::Draw( float dt )
+	{
+		this->_data->window.clear(sf::Color::Red);
+		this->_data->window.draw( this->_background );
+		this->_data->window.display();
+	}
+
+}
---
+++ src/HighscoreState.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "lib/SFML/Graphics.hpp"
+#include "State.hpp"
+#include "Game.hpp"
+
+namespace Hills
+{
+	class SplashState : public State
+	{
+		public:
+			HighscoreState(GameDataRef data);
+			void Init();
+			void HandleInput();
+			void Update( float dt );
+			void Draw( float dt );
+		private:
+			GameDataRef _data;
+			sf::Clock _clock;
+			sf::Sprite _background;
+	};
+}
