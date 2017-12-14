22a6566cf77fcdd7f96f02f942a0194d48a201af
Markus Toivonen <markus.toivonen@aalto.fi>
2017-12-14 16:04:17 +0200

    highscores now show top3 for 1 level

--- src/HighscoreState.cpp
+++ src/HighscoreState.cpp
@@ -6,6 +6,9 @@
 #include "DEFINITIONS.hpp"
 #include <iostream>
 #include <istream>
+#include <vector>
+#include <fstream>
+#include <algorithm>
 
 namespace Hills
 {
@@ -20,24 +23,51 @@ namespace Hills
 		this->_background.setTexture( this->_data->assets.GetTexture( "Splash State Background") );
 		this->_data->assets.LoadTexture( "Exit Button", EXIT_FILEPATH );
 		this->_exitButton.setTexture(this->_data->assets.GetTexture("Exit Button"));
-		std::string scores_string = "score1 \nscore2\nscore3";
-		// std::ifstream is("testhighscores");
-		// while(!is.eof())
-		// {
-		// 	std::getline(is,scores_string);
-		// 	scores_string += '\n';
-		// }
-		// is.close();
+		//std::string scores_string = "score1 \nscore2\nscore3";
+
+		std::vector<int> scoreVector;
+		std::string oneScore;
+		std::ifstream allScores;
+		std::stringstream convert;
+		std::vector<int> topScores;
+		int tmp;
+
+		allScores.open("allscores.txt");
+
+		while(!allScores.eof())
+		{
+			std::getline(allScores,oneScore);
+			convert << oneScore;
+			convert >> tmp;
+			scoreVector.push_back(tmp);
+			convert.str("");
+			convert.clear();
+		}
+		allScores.close();
+		std::sort (scoreVector.begin(),scoreVector.end());
+		int size = scoreVector.size();
+		int top1,top2,top3;
+		top1 = scoreVector[size-1];
+		top2 = scoreVector[size-2];
+		top3 = scoreVector[size-3];
+
+
 
 		if(!hsFont.loadFromFile("Resources/KhmerOS.ttf"))
 		{
 			std::cerr << "No font file found!" << std::endl;
 		}
-		std::cout << scores_string << std::endl;
+		std::cout << oneScore << std::endl;
 		scores.setFont(hsFont);
 		scores.setCharacterSize(20);
-		scores.setString(scores_string);
+		scores.setString("Level1 Highscores\n#1 " + std::to_string(top1) + "\n#2 " + std::to_string(top2) + "\n#3 " + std::to_string(top3));
 		scores.setPosition(SCREEN_WIDTH/2,SCREEN_HEIGHT/2);
+
+
+
+
+
+
 	}
 
 	void HighscoreState::HandleInput( )
